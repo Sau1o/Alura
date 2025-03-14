93999f2b8e319f2e0e4ae08d8cf1ccb164d72758
Saulo Coelho <coelho.saulo@gmail.com>
2025-03-14 10:54:26 -0300

    first commit

---
+++ condicionais/main.py
@@ -0,0 +1,7 @@
+qtd_maca = int(input('Digite a quantidade de maçãs vendidas: '))
+qtd_bananas = int(input('Digite a quantidade de bananas vendidas '))
+
+if (qtd_maca > qtd_bananas):
+    print('As maçãs tiveram mais vendas')
+else:
+    print('As bananas tiveram mais vendas')
